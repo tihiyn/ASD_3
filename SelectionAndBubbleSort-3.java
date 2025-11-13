442cc3c212c18b3e2ddaaf7c3c461f2ec2edb87b
tihiyn <tihiyn58067@gmail.com>
2025-11-13 09:45:01 +0300

    Realized one step of selection and bubble sorts

---
+++ SelectionAndBubbleSort-3.java
@@ -0,0 +1,68 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class SelectionAndBubbleSortTest {
+    @Test
+    void testSelectionSortForFirstElement() {
+        int[] input = new int[] {3, 9, 2, 6, 4};
+        int index = 0;
+        SelectionAndBubbleSorts.SelectionSortStep(input, index);
+        assertArrayEquals(new int[] {2, 9, 3, 6, 4}, input);
+    }
+
+    @Test
+    void testSelectionSortWithSwap() {
+        int[] input = new int[] {-77, -5, 24, 0};
+        int index = 2;
+        SelectionAndBubbleSorts.SelectionSortStep(input, index);
+        assertArrayEquals(new int[] {-77, -5, 0, 24}, input);
+    }
+
+    @Test
+    void testSelectionSortWithoutSwap() {
+        int[] input = new int[] {-12, 3, 18, 7, 110, 16};
+        int index = 1;
+        SelectionAndBubbleSorts.SelectionSortStep(input, index);
+        assertArrayEquals(new int[] {-12, 3, 18, 7, 110, 16}, input);
+    }
+
+    @Test
+    void testSelectionSortForLastElement() {
+        int[] input = new int[] {-3, -2, 2, 3};
+        int index = 3;
+        SelectionAndBubbleSorts.SelectionSortStep(input, index);
+        assertArrayEquals(new int[] {-3, -2, 2, 3}, input);
+    }
+
+    @Test
+    void testSelectionSortForEmptyArray() {
+        int[] input = new int[] {};
+        int index = 0;
+        SelectionAndBubbleSorts.SelectionSortStep(input, index);
+        assertArrayEquals(new int[] {}, input);
+    }
+
+    @Test
+    void testBubbleSortForNotSortedArray() {
+        int[] input = new int[] {4, 9, 2, -1, 0};
+        assertTrue(SelectionAndBubbleSorts.BubbleSortStep(input));
+        assertArrayEquals(new int[] {4, 2, -1, 0, 9}, input);
+    }
+
+    @Test
+    void testBubbleSortForSortedArray() {
+        int[] input = new int[] {3, 7, 10, 11, 11, 56};
+        assertFalse(SelectionAndBubbleSorts.BubbleSortStep(input));
+        assertArrayEquals(new int[] {3, 7, 10, 11, 11, 56}, input);
+    }
+
+    @Test
+    void testBubbleSortForEmptyArray() {
+        int[] input = new int[] {};
+        assertFalse(SelectionAndBubbleSorts.BubbleSortStep(input));
+        assertArrayEquals(new int[] {}, input);
+    }
+}
---
+++ SelectionAndBubbleSorts.java
@@ -0,0 +1,33 @@
+public class SelectionAndBubbleSorts {
+    public static void SelectionSortStep(int[] array, int i) {
+        if (i >= array.length - 1) {
+            return;
+        }
+        int minIdx = i + 1;
+        for (int j = i + 1; j < array.length; j++) {
+            if (array[j] < array[minIdx]) {
+                minIdx = j;
+            }
+        }
+        if (array[i] > array[minIdx]) {
+            swap(array, i, minIdx);
+        }
+    }
+
+    public static boolean BubbleSortStep(int[] array) {
+        boolean isSwapped = false;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                swap(array, i, i + 1);
+                isSwapped = true;
+            }
+        }
+        return isSwapped;
+    }
+
+    private static void swap(int[] array, int fst, int snd) {
+        array[fst] = -array[fst] - array[snd];
+        array[snd] = -array[snd] - array[fst];
+        array[fst] = -array[fst] - array[snd];
+    }
+}
