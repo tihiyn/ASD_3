8fcec60aa2198c0724bb6c99685424ff47ca4825
tihiyn <tihiyn58067@gmail.com>
2025-12-02 14:16:58 +0300

    Fixed test names

--- QuickSortTailOptimization/SortLevel-3.java
+++ QuickSortTailOptimization/SortLevel-3.java
@@ -1,41 +1,41 @@
+import Task_6.SortLevel;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class QuickSortTailOptimizationTest {
-    void testArrayChunk() {
+    @Test
+    void testQuickSortTailOptimization() {
         int[] input = new int[] {7, 5, 6, 4, 3, 1, 2};
         SortLevel.QuickSortTailOptimization(input, 0, input.length - 1);
         assertArrayEquals(new int[] {1, 2, 3, 4, 5, 6, 7}, input);
     }
 
     @Test
-    void testArrayChunkWithChangingOfPivot() {
+    void testQuickSortTailOptimizationWithChangingOfPivot() {
         int[] input = new int[] {5, -3, 0, 1, -6, -9, 24};
         SortLevel.QuickSortTailOptimization(input, 0, input.length - 1);
         assertArrayEquals(new int[] {-9, -6, -3, 0, 1, 5, 24}, input);
     }
 
     @Test
-    void testArrayChunkForOneElement() {
+    void testQuickSortTailOptimizationForOneElement() {
         int[] input = new int[] {7};
         SortLevel.QuickSortTailOptimization(input, 0, 0);
         assertArrayEquals(new int[] {7}, input);
     }
 
     @Test
-    void testArrayChunkForTwoNotOrderedElements() {
+    void testQuickSortTailOptimizationForTwoNotOrderedElements() {
         int[] input = new int[] {7, 3};
         SortLevel.QuickSortTailOptimization(input, 0, input.length - 1);
         assertArrayEquals(new int[] {3, 7}, input);
     }
 
     @Test
-    void testArrayChunkForEmpty() {
+    void testQuickSortTailOptimizationForEmpty() {
         int[] input = new int[] {};
         SortLevel.QuickSortTailOptimization(input, 0, 0);
         assertArrayEquals(new int[] {}, input);
     }
-
-
 }
